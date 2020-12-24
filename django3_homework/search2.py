d59b548df15d96a878b355a0f08427547fb6e7ce
kanghaoyu <18511580173>
2020-12-24 21:09:18 +0800

    add django3 work

---
+++ django3_homework/search2.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+from django.views.decorators import csrf
+
+
+# 接收POST请求数据
+from user.models import Test
+
+
+def search_post(request):
+    ctx = {}  #接受容器
+    if request.POST:
+        ctx['rlt'] = request.POST['q']
+        user1=Test(name=request.POST['q'])
+        user1.save()
+    return render(request, "post.html", ctx)
